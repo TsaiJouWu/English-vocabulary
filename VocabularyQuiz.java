bb59e6cf6327253b858d6d763a9ce7443d2c9b85
TsaiJouWu <136052379+TsaiJouWu@users.noreply.github.com>
2023-12-24 15:03:24 +0000

    feat: Select problem database function

--- VocabularyQuiz.java
+++ VocabularyQuiz.java
@@ -17,14 +17,28 @@ public class VocabularyQuiz extends JFrame {
     private JTextField inputField;
     private JButton submitButton;
     private JLabel resultLabel;
+    private JComboBox<String> apiSelector; 
 
     public VocabularyQuiz() {
-        loadWordsFromJson();
-
         wordLabel = new JLabel();
         inputField = new JTextField();
         submitButton = new JButton("Submit");
         resultLabel = new JLabel();
+        apiSelector = new JComboBox<>();
+
+        String[] apiOptions = {"API 1", "API 2", "API 3", "API 4", "API 5", "API 6"};
+        apiSelector.setModel(new DefaultComboBoxModel<>(apiOptions));
+        apiSelector.setSelectedIndex(0);
+
+        apiSelector.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currentIndex = 0;
+                loadWordsFromJson(); 
+                setNextQuestion(); 
+            }
+        });
+        
 
         submitButton.addActionListener(new ActionListener() {
             @Override
@@ -33,10 +47,9 @@ public class VocabularyQuiz extends JFrame {
             }
         });
 
-        setNextQuestion();
-
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 1, 10, 10));
+        panel.setLayout(new GridLayout(5, 1, 10, 10));
+        panel.add(apiSelector);
         panel.add(wordLabel);
         panel.add(inputField);
         panel.add(submitButton);
@@ -49,19 +62,54 @@ public class VocabularyQuiz extends JFrame {
         setTitle("Vocabulary Quiz");
         setLocationRelativeTo(null);
         setVisible(true);
+
+        // 初始載入題庫
+        loadWordsFromJson();
+        setNextQuestion();
     }
 
     private void loadWordsFromJson() {
         try {
+            String selectedApi = (String) apiSelector.getSelectedItem();
+            // if (selectedApi == null || selectedApi.equals("Select API")) {
+
+            //     selectedApi = "API 1";
+            //     apiSelector.setSelectedItem(selectedApi);
+            // }
+
+            String apiEndpoint = getApiEndpoint(selectedApi);
+
             java.lang.reflect.Type listType = TypeToken.getParameterized(List.class, Word.class).getType();
-            URL url = new URL("https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/1%E7%B4%9A.json");
+            URL url = new URL(apiEndpoint);
             InputStreamReader reader = new InputStreamReader(url.openStream());
             words = new Gson().fromJson(reader, listType);
+
+            currentIndex = 0;
+            setNextQuestion();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     
+    private String getApiEndpoint(String apiName) {
+        switch (apiName) {
+            case "API 1":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/1%E7%B4%9A.json";
+            case "API 2":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/2%E7%B4%9A.json";
+            case "API 3":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/3%E7%B4%9A.json";
+            case "API 4":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/4%E7%B4%9A.json";
+            case "API 5":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/5%E7%B4%9A.json";
+            case "API 6":
+                return "https://raw.githubusercontent.com/AppPeterPan/TaiwanSchoolEnglishVocabulary/main/6%E7%B4%9A.json";
+            default:
+                return "";
+        }
+    }
+
     private void setNextQuestion() {
         if (currentIndex < words.size()) {
             Word currentWord = words.get(currentIndex);
@@ -69,15 +117,15 @@ public class VocabularyQuiz extends JFrame {
             inputField.setText("");
             submitButton.setText("Submit");
             resultLabel.setText("");
-    
+
             for (ActionListener listener : submitButton.getActionListeners()) {
                 submitButton.removeActionListener(listener);
             }
-    
+
             submitButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    checkAnswer(); // 在切換到下一題時再次檢查答案
+                    checkAnswer(); 
                 }
             });
         } else {
@@ -87,24 +135,22 @@ public class VocabularyQuiz extends JFrame {
         }
     }
     
-
     private void checkAnswer() {
         Word currentWord = words.get(currentIndex);
         String userAnswer = inputField.getText().trim().toLowerCase();
         String correctAnswer = currentWord.getWord().toLowerCase();
-    
+
         if (userAnswer.isEmpty()) {
-            // 使用者未輸入答案
             resultLabel.setText("<html><font color='red'>Please enter an answer.</font></html>");
         } else {
             if (userAnswer.equals(correctAnswer)) {
                 resultLabel.setText("<html><font color='green'>Correct! Pronunciation: " + currentWord.getPronunciation() + "</font></html>");
                 submitButton.setText("Next Question");
-    
+
                 for (ActionListener listener : submitButton.getActionListeners()) {
                     submitButton.removeActionListener(listener);
                 }
-    
+
                 submitButton.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -122,7 +168,7 @@ public class VocabularyQuiz extends JFrame {
     private void shakeTextField() {
         final int numberOfShakes = 3;
         final int deltaX = 10;
-        Timer timer = new Timer(100, new ActionListener() {
+        Timer timer = new Timer(500, new ActionListener() {
             int shakes = numberOfShakes;
 
             @Override
